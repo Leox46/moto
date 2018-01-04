55eae523e276744675a93ee03ef9784a9d54756b
Leox46 <drleonardo46@gmail.com>
2018-01-04 15:04:33 +0100

    first commit

---
+++ moto.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// ci sono solo 3 data type: Object, String e Number.
+var MotoSchema = new Schema({
+	motoId: String,
+	number: Number,
+	manufactor: String,
+	model: String,
+	displacement: Number,
+	power: String
+});
+
+module.exports = mongoose.model('Moto', MotoSchema);
